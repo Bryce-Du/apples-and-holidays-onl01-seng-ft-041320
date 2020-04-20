97be172693e8469dccd8dca8b03dc47f01d2919e
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-20 15:05:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holidays|
+    puts season.to_s + ":"
+    holidays.each do |holiday, supplies|
+        
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
