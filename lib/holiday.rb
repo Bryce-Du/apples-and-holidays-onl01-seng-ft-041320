7afab24a86a14c08cecf7301f61a4f0e450f0fa6
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-20 15:23:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts season.to_s.capitalize! + ":"
     holidays.each do |holiday, supplies|
       puts "new_years".split
-      holiday.to_s.split
+      holiday.to_s.split("_")
       puts "  " + final_holiday + ": " + supplies.join(", ")
     end
   end
